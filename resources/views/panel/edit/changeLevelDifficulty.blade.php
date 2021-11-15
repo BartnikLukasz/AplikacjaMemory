22723eccd3564d1c7d4b493f364e66acd57f8653
KacBer <berlinski.kacper@gmail.com>
2021-11-15 16:29:54 +0100

    Poprawa wizualna okna edycji mnożnika

--- resources/views/panel/edit/changeLevelDifficulty.blade.php
+++ resources/views/panel/edit/changeLevelDifficulty.blade.php
@@ -1,23 +1,25 @@
 <x-app-layout>
-    <h3 class="mb-3 text-center">Zmiana poziomu {{ $oldLevelDifficulty->level }}</h3>
-        <form method="POST" action="{{ route('changeLevelDifficulty') }}">
-            @csrf
+    <div class="main-panel">
+        <h3 class="mb-3 text-center">Zmiana mnożnika poziomu {{ $oldLevelDifficulty->level }}</h3>
+            <form method="POST" action="{{ route('changeLevelDifficulty') }}">
+                @csrf
 
-            <input type='hidden' name="id" class="d-inline" value="{{ $oldLevelDifficulty->id }}" />
+                <input type='hidden' name="id" class="d-inline" value="{{ $oldLevelDifficulty->id }}" />
 
-            <label for="multiplier" class="mt-2">Mnożnik punktów</label><br>
-            <input id="multiplier" type="number" class="@error('multiplier') is-invalid @enderror" name="multiplier" value="{{ $oldLevelDifficulty->multiplier }}" required autocomplete="multiplier"><br>
+                <label for="multiplier" class="mt-2">Mnożnik punktów</label><br>
+                <input id="multiplier" type="number" class="@error('multiplier') is-invalid @enderror w-100" name="multiplier" value="{{ $oldLevelDifficulty->multiplier }}" required autocomplete="multiplier"><br>
 
-            @error('multiplier')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                @error('multiplier')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
 
-            <input type="submit" value="Zmień poziom" class="button mt-4">
+                <input type="submit" value="Zmień mnożnik" class="button mt-4">
 
-        </form>
-    <a class="btn btn-primary" href="{{ route('dashboard') }}">
-        {{ __('Powrót') }}
-    </a>
+            </form>
+        <a class="back-button-container text-center" href="{{ route('controlPanelLevelDifficulties') }}">
+            <div class="back-button button">{{ __('Powrót') }}</div>
+        </a>
+    </div>
 </x-app-layuout>
