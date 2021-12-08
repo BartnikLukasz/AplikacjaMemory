cad82bab5fee573a285076b76d63b39ddf576f1b
= <=>
2021-12-08 18:54:01 +0100

    Poprawka ekranu błędu

--- resources/views/errors/default.blade.php
+++ resources/views/errors/default.blade.php
@@ -2,7 +2,7 @@
     <div>
         <h2>Wystąpił błąd</h2>
     </div>
-    <a class="btn btn-primary" href="{{ route('dashboard') }}">
-        {{ __('Powrót do menu') }}
+    <a class="btn btn-primary" href="{{ route('login') }}">
+        {{ __('Powrót do logowania') }}
     </a>
 </x-app-layuout>
